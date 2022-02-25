ad5d3a113a1249f78f27916d048d5579bb6f2a67
DarkedgeTH <darkedgeth@gmail.com>
2022-02-24 20:48:22 -0500

    added onChange function and started setting up return for PizzaForm.js

--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -1,5 +1,27 @@
 import React from 'react';
 
 export default function PizzaForm(props) {
+    const { values, update, submit } = props
 
+    const onChange = evt => {
+        const name = evt.target.name;
+        const value = evt.target.value;
+        update(name, value);
+    }
+
+    return (
+        <form className='pizza-form'>
+            <div className='form-group inputs'>
+                <label>Name
+                    <input className='name-input'
+                        name='name'
+                        type='text'
+                        placeholder='Enter your name.'
+                        value={values.name}
+                        onChange={onChange}
+                    />
+                </label>
+            </div>
+        </form>
+    )
 }
