3805d647d067f5bf287d8780d125ce48ed0f5ba3
DarkedgeTH <darkedgeth@gmail.com>
2022-02-25 09:48:45 -0500

    added order-pizza route and PizzaForm.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Route, Link } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
+      <div>
+        <Link id='order-pizza' to='/'>Order Pizza</Link>
+      </div>
+
+      <Route path='/'>
+
+      </Route>
     </>
   );
 };
