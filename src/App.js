c96cd5a40e36a8f662f5311daa3e5c192c0a23db
DarkedgeTH <darkedgeth@gmail.com>
2022-02-24 21:43:02 -0500

    starting over

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
-import React, { useState } from "react";
-import axios from 'axios';
+import React from "react";
 
-import PizzaForm from './PizzaForm';
-
-const initialFormValues = {
-  name: '',
-  size: '',
-  topping1: false,
-  topping2: false,
-  special: ''
-}
-
-function App () {
-
-  const [formValues, setFormValues] = useState(initialFormValues)
-
-  const updateForm = (inputName, inputValue) => {
-    setFormValues({ ...formValues, [inputName]: inputValue });
-  }
-
-  const submitForm = () => {
-    axios.post('https://reqres.in/api/orders', formValues)
-      .then(res => {
-        console.log(res)
-      })
-      .catch(err => console.error(err))
-      .finally(() => setFormValues(initialFormValues))
-  }
-
+const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
-      <PizzaForm values={formValues} update={updateForm} submit={submitForm} />
     </>
   );
 };
-
 export default App;
