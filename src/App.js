6458195b8db7d42e0219f036ea03dddab11b8ed8
DarkedgeTH <darkedgeth@gmail.com>
2022-02-25 10:08:47 -0500

    imported useState and created formValues and intialFormValues

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Link } from 'react-router-dom';
 
 import PizzaForm from './PizzaForm'
 
+const initialFormValues = {
+  name: '',
+  size: '',
+  topping1: false,
+  topping2: false,
+  special: ''
+}
+
 const App = () => {
+
+  const [formValues, setFormValues] = useState(initialFormValues)
+
   return (
     <>
       <h1>Lambda Eats</h1>
@@ -13,7 +24,7 @@ const App = () => {
       </div>
 
       <Route path='/pizza'>
-        <PizzaForm />
+        <PizzaForm values={formValues} />
       </Route>
     </>
   );
